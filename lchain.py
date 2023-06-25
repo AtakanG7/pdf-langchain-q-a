a5b79926c40a69eff1f7739f3135263f9039a272
Atakan Gul <pwxcv7352@gmail.com>
2023-06-26 01:15:05 +0300

    Initial commit

---
+++ lchain.py
@@ -0,0 +1,83 @@
+import os
+from langchain.llms import OpenAI
+from langchain.agents import initialize_agent, AgentType
+from langchain.agents import load_tools
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+import streamlit as st
+from PyPDF2 import PdfReader
+
+os.environ["SERPAPI_API_KEY"] = "1757bebc765c4c4d7867c3b99adb905e7f78d80d791b119baed0ba87d033d3d3"
+os.environ["OPENAI_API_KEY"] = "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"
+
+template =  """ Given the text: "{query}".
+
+    -Based on the provided text, generate 5 questions and answer options (a,b,c and d):
+    -Generate these questions with respect to context of the given text input
+
+"""
+
+         
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["query"] 
+)                                                         
+
+def main():
+    
+    
+    st.header("Generate Questions From Your PDF")
+    with st.sidebar:
+        st.image("./image/summarify-logo-300.png")
+        
+        "contacts"
+        st.text("http:/summarify.io")
+        
+    pdf = st.file_uploader("Upload your PDF file here", accept_multiple_files=False)
+    
+    temperature = st.slider(
+    'How much our AI model should be deterministic?',
+    0, 10, 1)   
+    st.caption("0 recommended")
+    
+    llm = OpenAI(temperature=temperature/10,model_name="text-davinci-003",openai_api_key=os.getenv("OPENAI_API_KEY"))       
+    
+    
+    if pdf is not None:
+        
+        readPDF = PdfReader(pdf)
+        text = ""
+        
+        for page in readPDF.pages:
+            text += page.extract_text()
+            
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2800,
+            chunk_overlap=50,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+        st.write("---")
+        st.write(llm(prompt.format(query=chunks[(int)(len(chunks)/2):5])))
+        st.write("---")
+        
+        
+            
+        
+        
+        
+        
+        
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
