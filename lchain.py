33b327063d564fb8605e20a75ac09b08686a3a68
Atakan Gul <pwxcv7352@gmail.com>
2023-07-18 18:08:42 +0300

    .

--- lchain.py
+++ lchain.py
@@ -3,37 +3,37 @@ from langchain.llms import OpenAI
 from langchain.agents import initialize_agent, AgentType
 from langchain.agents import load_tools
 from langchain.prompts import PromptTemplate
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 import streamlit as st
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from PyPDF2 import PdfReader
+import pickle
+from dotenv import load_dotenv
 
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "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"
+key = os.getenv("OPENAI_API_KEY") 
 
-template =  """ Given the text: "{query}".
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = []
 
-    -Based on the provided text, generate 5 questions and answer options (a,b,c and d):
-    -Generate these questions with respect to context of the given text input
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
 
-"""
-
-         
-prompt = PromptTemplate(
-    template=template,
-    input_variables=["query"] 
-)                                                         
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory= ConversationBufferWindowMemory(k=3)
 
 def main():
     
     
-    st.header("Generate Questions From Your PDF")
+    st.header("Chat With Your Documents💬")
+
     with st.sidebar:
         st.image("./image/summarify-logo-300.png")
-        
         "contacts"
         st.text("http:/summarify.io")
         
@@ -44,26 +44,63 @@ def main():
     0, 10, 1)   
     st.caption("0 recommended")
     
-    llm = OpenAI(temperature=temperature/10,model_name="text-davinci-003",openai_api_key=os.getenv("OPENAI_API_KEY"))       
+    llm = OpenAI(temperature=temperature/10,model_name="gpt-3.5-turbo",openai_api_key=key)       
     
     
     if pdf is not None:
-        
-        readPDF = PdfReader(pdf)
-        text = ""
-        
-        for page in readPDF.pages:
-            text += page.extract_text()
+        name = pdf.name
+
+        if not os.path.exists(name):
+
+            readPDF = PdfReader(pdf)
+            text = ""
+
+            question = st.text_input("Enter",key="input...")
+            for page in readPDF.pages:
+                text += page.extract_text()
+                
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=2800,
+                chunk_overlap=50,
+                length_function=len
+            )
+
+            chunks = text_splitter.split_text(text=text)
+            embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+            vectorStore = FAISS.from_documents(chunks,embedding=embeddings)
+
+            with open(f'{name}.pkl', 'wb') as file:
+                pickle.dump(textSearch, file)
+
+            qa = RetrievalQA.from_chain_type(llm,chain_type="stuff", retriever=vectorStore.as_retriever(), memory=st.session_state.buffer_memory)
+
+            st.write("---")
+
+            st.write(qa.run(question))
+
+            st.write("---")
+
+        else:
+
+                readPDF = PdfReader(pdf)
+                text = ""
+
+                question = st.text_input("Enter",key="input...")
+                for page in readPDF.pages:
+                    text += page.extract_text()
+                
+                with open("./langchain.cpython-39.pyc","rb") as file:
+                    textSearch = pickle.load(file)
+
+                qa = RetrievalQA.from_chain_type(llm,chain_type="stuff", retriever=textSearch.as_retriever(), memory=st.session_state.buffer_memory)
+
+                st.write("---")
+
+                st.write(qa.run(question))
+
+                st.write("---")
             
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=2800,
-            chunk_overlap=50,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text=text)
-        st.write("---")
-        st.write(llm(prompt.format(query=chunks[(int)(len(chunks)/2):5])))
-        st.write("---")
+
         
         
             
